03dbd34338106f5f5cdeb361e7cabc0198d87ce8
Idalecio Bras <idaleciobraz399@gmail.com>
2024-05-17 12:11:05 +0100

    Refactor alter. meta title

--- script/contact.php
+++ script/contact.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Archia - Architecture & Interior HTML Templates
+Template Name: Arte Digitals
 
 Variable
 	$recaptchaSecret : Recaptcha Secret Key
--- script/mailchamp.php
+++ script/mailchamp.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Archia - Architecture & Interior HTML Template
+Template Name: Arte Digital
 
 Variable
 	$apiKey : Mail Champ Api
